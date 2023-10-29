39442ae67a35ab81368b03425eab555a11a95d25
Kento1911 <ks99711041@gmail.com>
2023-10-30 00:07:17 +0900

    parameter fix

--- src/main/java/org/example/entity/XmlTestReader.java
+++ src/main/java/org/example/entity/XmlTestReader.java
@@ -8,15 +8,14 @@ import org.w3c.dom.NodeList;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import java.io.File;
-import java.io.FileReader;
-import java.io.Reader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class XmlTestReader {
     DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
     List<String> propertiesList = new ArrayList<>();
 
-    public void nodeReader(File xmlPath, String ProertiesPath) {
+    public void nodeReader(File xmlPath) {
         try {
             DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
             Document document = documentBuilder.parse(xmlPath);
@@ -26,16 +25,16 @@ public class XmlTestReader {
             for (int i = 0; i < nodeList.getLength(); i++) {
                 Element infomation = (Element) nodeList.item(i);
                 NodeList childlen = infomation.getChildNodes();
-//                System.out.println(infomation.getNodeName() + ":" + infomation.getTextContent());
                 for (int j = 0; j < childlen.getLength(); j++) {
                     Node innerData = childlen.item(j);
-                    if(innerData instanceof Element innerDataElement){
+                    if (innerData instanceof Element innerDataElement) {
                         System.out.println(innerDataElement.getNodeName() + ":" + innerDataElement.getTextContent());
                     }
                 }
                 System.out.println();
             }
         } catch (Exception e) {
+
             throw new RuntimeException(e);
         }
     }
